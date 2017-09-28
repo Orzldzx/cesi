9962c48571555f4726b74d7b79ae9284ed5fded1
Orzldzx <Orzldzx@gmail.com>
2017-09-28 10:20:39 +0800

    增加备注

--- cesi/cesi.py
+++ cesi/cesi.py
@@ -1,24 +1,27 @@
+# -*- coding: utf-8 -*-
+
 import xmlrpclib
 import ConfigParser
-import requests
 from datetime import datetime, timedelta
 from flask import jsonify
 
 CONFIG_FILE = "/etc/cesi.conf"
 class Config:
-    
+
     def __init__(self, CFILE):
+        '''
+        读配置文件: [<类型>:<名字>]
+            取类型名称,比如:
+            主机名, 组名, 环境名
+        '''
         self.CFILE = CFILE
         self.cfg = ConfigParser.ConfigParser()
         self.cfg.read(self.CFILE)
-        self.etcd_nodes = {}
 
         self.node_list = []
         for name in self.cfg.sections():
             if name[:4] == 'node':
                 self.node_list.append(name[5:])
-            elif name == 'etcd':
-                self.processEtcdNodes()
 
         self.environment_list = []
         for name in self.cfg.sections():
@@ -30,100 +33,130 @@ class Config:
             if name[:5] == 'group':
                 self.group_list.append(name[6:])
 
-    def processEtcdNodes(self):
-        r = requests.get(self.cfg.get('etcd', 'discovery') + 'v2/keys' + self.cfg.get('etcd', 'key') + '?recursive=true')
-        etcd_nodes = r.json()
-        if 'node' in etcd_nodes.keys() and 'nodes' in etcd_nodes['node'].keys():
-            for spvs in etcd_nodes['node']['nodes']:
-                if 'nodes' in spvs.keys():
-                    for data_node in spvs['nodes']:
-                        hostname = None
-                        name = None
-                        port = None
-                        if data_node['key'].endswith('name'):
-                            name = data_node['value']
-
-                        if data_node['key'].endswith('hostname'):
-                            hostname = data_node['value']
-
-                        if data_node['key'].endswith('port'):
-                            port = data_node['value']
-
-                        if hostname is not None and name is not None:
-                            self.etcd_nodes[name] = NodeConfig(name, hostname, port, '', '' )
-                            self.node_list.append(name)
 
     def getNodeConfig(self, node_name):
-        self.node_name = "node:%s" % (node_name)
-        self.node_config = None
-        if self.cfg.has_section(self.node_name):     
-            self.username = self.cfg.get(self.node_name, 'username')
-            self.password = self.cfg.get(self.node_name, 'password')
-            self.host = self.cfg.get(self.node_name, 'host')
-            self.port = self.cfg.get(self.node_name, 'port')
-            self.node_config = NodeConfig(self.node_name, self.host, self.port, self.username, self.password)
-        elif self.node_name in self.etcd_nodes:
-            self.node_config = self.etcd_nodes[self.node_name]
-
+        '''
+        通过主机名称来查找主机详细信息
+        '''
+        self.node_name = "node:%s" % (node_name)                    # node:<参数>
+        self.username = self.cfg.get(self.node_name, 'username')    # conf 里 node 下的用户名
+        self.password = self.cfg.get(self.node_name, 'password')    # 密码
+        self.host = self.cfg.get(self.node_name, 'host')            # ip地址
+        self.port = self.cfg.get(self.node_name, 'port')            # 端口
+        self.node_config = NodeConfig(self.node_name, self.host, self.port, self.username, self.password)
+        # 返回类属性: self.node_name = 'node:server1', self.host = '192.168.0.85', self.port = 9001, self.username = 'test', self.password = 'test'
         return self.node_config
 
     def getMemberNames(self, environment_name):
+        '''
+        通过环å¢名称来获取下属主机列表
+        '''
         self.environment_name = "environment:%s" % (environment_name)
         self.member_list = self.cfg.get(self.environment_name, 'members')
         self.member_list = self.member_list.split(', ')
+        # 返回列表: self.member_list = [ 'server1', 'server2', 'server3' ]
         return self.member_list
 
     def getDatabase(self):
+        '''
+        获取数据库地址
+        '''
         return str(self.cfg.get('cesi', 'database'))
 
     def getActivityLog(self):
+        '''
+        获取日志地址
+        '''
         return str(self.cfg.get('cesi', 'activity_log'))
 
     def getHost(self):
+        '''
+        获取监听主机
+        '''
         return str(self.cfg.get('cesi', 'host'))
 
-    def getBindPort(self):
-        return str(self.cfg.get('cesi', 'port'))
-
 class NodeConfig:
-
+    '''
+    主机信息加载到类, 方便调用
+    '''
     def __init__(self, node_name, host, port, username, password):
         self.node_name = node_name
         self.host = host
         self.port = port
         self.username = username
         self.password = password
-            
+
 
 class Node:
-
+    '''
+    通过主机信息来获取该主机进程列表
+    '''
     def __init__(self, node_config):
-        self.long_name = node_config.node_name
-        self.name = node_config.node_name[5:]
-        self.connection = Connection(node_config.host, node_config.port, node_config.username, node_config.password).getConnection()
+        self.long_name = node_config.node_name      # 'node:server1'
+        self.name = node_config.node_name[5:]       # server1
+        self.connection = Connection(node_config.host, node_config.port, node_config.username, node_config.password).getConnection()    # 连接上xmlrpc
         self.process_list=[]
         self.process_dict2={}
-        for p in self.connection.supervisor.getAllProcessInfo():
-            self.process_list.append(ProcessInfo(p))
-            self.process_dict2[p['group']+':'+p['name']] = ProcessInfo(p)
-        self.process_dict = self.connection.supervisor.getAllProcessInfo()
+        for p in self.connection.supervisor.getAllProcessInfo():            # 获取主机上的全部进程信息
+            self.process_list.append(ProcessInfo(p))                        # 某个进程信息列表
+            self.process_dict2[p['group']+':'+p['name']] = ProcessInfo(p)   # { '进程组名:进程名': '进程信息' }
+        self.process_dict = self.connection.supervisor.getAllProcessInfo()  # 获取主机上的全部进程信息
 
 
 class Connection:
-
+    '''
+    连接 supervisorctl 管理进程
+    '''
     def __init__(self, host, port, username, password):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
-        self.address = "http://%s:%s@%s:%s/RPC2" %(self.username, self.password, self.host, self.port)
+        self.address = "http://%s:%s@%s:%s/RPC2" %(self.username, self.password, self.host, self.port)  # 'http://test:test@192.168.0.85:9001/RPC2'
 
     def getConnection(self):
+        # 返回 <ServerProxy for 192.168.0.85:9001/RPC2> 对象
         return xmlrpclib.Server(self.address)
-        
+
 
 class ProcessInfo:
-
+    '''
+    格式化某个进程的信息:
+    {
+        'description': 'pid 29367, uptime 8:12:50',
+        'exitstatus': 0,
+        'group': 'test',
+        'logfile': '/var/log/supervisor/test.log',
+        'name': 'test',
+        'now': 1506506033,
+        'pid': 29367,
+        'spawnerr': '',
+        'start': 1506476463,
+        'state': 20,
+        'statename': 'RUNNING',
+        'stderr_logfile': '/var/log/supervisor/test.log',
+        'stdout_logfile': '/var/log/supervisor/test.log',
+        'stop': 0
+    }
+    self.dictionary = dictionary
+    self.name = 'test'
+    self.group = 'test'
+    self.start = 1506476463     # 启动时间, 时间戳
+    self.start_hr = 把时间戳转换成 %H:%M:%S
+    self.stop_hr =              # 停止时间
+停止时间¶间
+    self.stop = 0
+    self.now = 1506506033
+    self.state = 20
+    self.statename = 'RUNNING'
+    self.spawnerr = ''
+    self.exitstatus = 0
+    self.stdout_logfile = '/var/log/supervisor/test.log'
+    self.stderr_logfile = '/var/log/supervisor/test.log'
+    self.pid = 29367
+    self.seconds = self.now - self.start    # 运行时长 (当前时间减去启动时间)
+    self.uptime = 格式化成字符串
+    '''
     def __init__(self, dictionary):
         self.dictionary = dictionary
         self.name = self.dictionary['name']
@@ -145,7 +178,9 @@ class ProcessInfo:
         self.uptime = str(timedelta(seconds=self.seconds))
 
 class JsonValue:
-    
+    '''
+    通过进程名称, 主机名称, 事件来获取和该主机有关的一切
+    '''
     def __init__(self, process_name, node_name, event):
         self.process_name = process_name
         self.event = event
@@ -154,13 +189,15 @@ class JsonValue:
         self.node = Node(self.node_config)
 
     def success(self):
+        '事件触发成功'
         return jsonify(status = "Success",
                        code = 80,
                        message = "%s %s %s event succesfully" %(self.node_name, self.process_name, self.event),
                        nodename = self.node_name,
                        data = self.node.connection.supervisor.getProcessInfo(self.process_name))
 
-    def error(self, code, payload):     
+    def error(self, code, payload):
+        '事件触发失败'
         self.code = code
         self.payload = payload
         return jsonify(status = "Error",
@@ -168,5 +205,4 @@ class JsonValue:
                        message = "%s %s %s event unsuccesful" %(self.node_name, self.process_name, self.event),
                        nodename = self.node_name,
                        payload = self.payload)
- 
 
--- cesi/web.py
+++ cesi/web.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, url_for, redirect, jsonify, request, g, session, flash
 from cesi import Config, Connection, Node, CONFIG_FILE, ProcessInfo, JsonValue
 from datetime import datetime
-import cesi 
+import cesi
 import xmlrpclib
 import sqlite3
 import mmap
@@ -12,13 +14,13 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key= '42'
 
-DATABASE = Config(CONFIG_FILE).getDatabase()
-ACTIVITY_LOG = Config(CONFIG_FILE).getActivityLog()
-HOST = Config(CONFIG_FILE).getHost()
-PORT = int(Config(CONFIG_FILE).getBindPort())
+DATABASE = Config(CONFIG_FILE).getDatabase()        # 获取数据库地址
+ACTIVITY_LOG = Config(CONFIG_FILE).getActivityLog() # 获取日志地址
+HOST = Config(CONFIG_FILE).getHost()                # 获取监听地址
 
 # Database connection
 def get_db():
+    '连接数据库'
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
@@ -27,10 +29,12 @@ def get_db():
 # Close database connection
 @app.teardown_appcontext
 def close_connection(exception):
+    '关闭数据库'
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+# 输出日志
 @app.route('/activitylog')
 def getlogtail():
     n=12
@@ -56,11 +60,11 @@ def getlogtail():
         except (UnboundLocalError, TypeError):
             return jsonify(status="error",
                            message = "Activity log file is empty")
-            
 
 
 
-# Username and password control
+
+# Username and password control (登录)
 @app.route('/login/control', methods = ['GET', 'POST'])
 def control():
     if request.method == 'POST':
@@ -72,9 +76,9 @@ def control():
         if not cur.fetchall():
             session.clear()
             add_log = open(ACTIVITY_LOG, "a")
-            add_log.write("%s - Login fail. Username does not exists.\n"%( datetime.now().ctime() ))
+            add_log.write("%s - Login fail. Username is not avaible.\n"%( datetime.now().ctime() ))
             return jsonify(status = "warning",
-                           message = "Username or password are incorrect.")
+                           message = "Username is not  avaible ")
         else:
             cur.execute("select * from userinfo where username=?",(username,))
             if password == cur.fetchall()[0][1]:
@@ -90,14 +94,14 @@ def control():
                 add_log = open(ACTIVITY_LOG, "a")
                 add_log.write("%s - Login fail. Invalid password.\n"%( datetime.now().ctime() ))
                 return jsonify(status = "warning",
-                               message = "Username or password are incorrect.")
+                               message = "Invalid password")
 
 # Render login page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-# Logout action
+# Logout action (注销)
 @app.route('/logout', methods = ['GET', 'POST'])
 def logout():
     add_log = open(ACTIVITY_LOG, "a")
@@ -108,7 +112,7 @@ def logout():
 # Dashboard
 @app.route('/')
 def showMain():
-# get user type
+    # 获取用户类型
     if session.get('logged_in'):
         if session['usertype']==0:
             usertype = "Admin"
@@ -118,59 +122,63 @@ def showMain():
             usertype = "Only Log"
         elif session['usertype']==3:
             usertype = "Read Only"
- 
-        all_process_count = 0
-        running_process_count = 0
-        stopped_process_count = 0
-        member_names = []
-        environment_list = []
-        g_node_list = []
-        g_process_list = []
-        g_environment_list = []
-        group_list = []
-        not_connected_node_list = []
-        connected_node_list = []
 
-        node_name_list = Config(CONFIG_FILE).node_list
-        node_count = len(node_name_list)
-        environment_name_list = Config(CONFIG_FILE).environment_list
-        
+        all_process_count = 0               # 进程总数
+        running_process_count = 0           # 运行状态进程总数
+        stopped_process_count = 0           # 停止状态进程总数
+        member_names = []                   # 环境所属主机列表
+        environment_list = []               # 环境列表
+        g_node_list = []                    # 组所属主机列è¡¨
+        g_process_list = []                 # 组所属进程列表
+        g_environment_list = []             # 组所属环境列表
+        group_list = []                     # 组列表
+        not_connected_node_list = []        # 连接失败主机列表
+        connected_node_list = []            # 连接成功主机列表
+
+        node_name_list = Config(CONFIG_FILE).node_list                  # 所有主机列表
+        node_count = len(node_name_list)                                # 连接成功主机
+        environment_name_list = Config(CONFIG_FILE).environment_list    # 环境列表
+
 
         for nodename in node_name_list:
+            '''
+            查询主机详细信息,
+            类属性: self.node_name = 'node:server1', self.host = '192.168.0.85', self.port = 9001, self.username = 'test', self.password = 'test'
+            '''
             nodeconfig = Config(CONFIG_FILE).getNodeConfig(nodename)
 
             try:
-                node = Node(nodeconfig)
-                if not nodename in connected_node_list:
+                node = Node(nodeconfig)                     # 进程列表
+                if not nodename in connected_node_list:     # 如果主机连接成功添加到列表
                     connected_node_list.append(nodename);
             except Exception as err:
-                 if not nodename in not_connected_node_list:
+                if not nodename in not_connected_node_list: # 否则添加到连接失败列表
                     not_connected_node_list.append(nodename);
-                 continue
+                continue
 
-            for name in node.process_dict2.keys():
+            for name in node.process_dict2.keys():      # { '进程组名:进程名': '进程信息' }
                 p_group = name.split(':')[0]
                 p_name = name.split(':')[1]
-                if p_group != p_name:
+                if p_group != p_name:                   # 如果进程组不在列表里则添加
                     if not p_group in group_list:
                         group_list.append(p_group)
 
-            for process in node.process_list:
-                all_process_count = all_process_count + 1
+            for process in node.process_list:                           # 本机所以进程
+                all_process_count = all_process_count + 1               # 总进程加1
                 if process.state==20:
-                    running_process_count = running_process_count + 1
+                    running_process_count = running_process_count + 1   # 运行进程加1
                 if process.state==0:
-                    stopped_process_count = stopped_process_count + 1
+                    stopped_process_count = stopped_process_count + 1   # 停止进程加1
 
-        # get environment list 
+        # 获取环境主机列表
         for env_name in environment_name_list:
             env_members = Config(CONFIG_FILE).getMemberNames(env_name)
             for index, node in enumerate(env_members):
                 if not node in connected_node_list:
                     env_members.pop(index);
-            environment_list.append(env_members)        
-                    
-        
+            environment_list.append(env_members)
+
+        # 获取进程组所属主机列表和其所属进程列表
         for g_name in group_list:
             tmp= []
             for nodename in connected_node_list:
@@ -183,6 +191,7 @@ def showMain():
                             tmp.append(nodename)
             g_node_list.append(tmp)
 
+        # 获取进程组所属环境下的主机
         for sublist in g_node_list:
             tmp = []
             for name in sublist:
@@ -192,10 +201,11 @@ def showMain():
                             if not env_name in tmp:
                                 tmp.append(env_name)
             g_environment_list.append(tmp)
-        
+
         connected_count = len(connected_node_list)
         not_connected_count = len(not_connected_node_list)
 
+        # 返回数据给模板渲染前端页面
         return render_template('index.html',
                                 all_process_count =all_process_count,
                                 running_process_count =running_process_count,
@@ -213,7 +223,7 @@ def showMain():
                                 username = session['username'],
                                 usertype = usertype,
                                 usertypecode = session['usertype'])
-    else:   
+    else:
         return redirect(url_for('login'))
 
 
@@ -224,7 +234,7 @@ def showNode(node_name):
         node_config = Config(CONFIG_FILE).getNodeConfig(node_name)
         add_log = open(ACTIVITY_LOG, "a")
         add_log.write("%s - %s viewed node %s .\n"%( datetime.now().ctime(), session['username'], node_name ))
-        return jsonify( process_info = Node(node_config).process_dict) 
+        return jsonify( process_info = Node(node_config).process_dict)
     else:
         add_log = open(ACTIVITY_LOG, "a")
         add_log.write("%s - Illegal request for view node %s .\n"%( datetime.now().ctime(), node_name ))
@@ -257,59 +267,60 @@ def showGroup(group_name, environment_name):
         return redirect(url_for('login'))
 
 
+# 重启进程
 @app.route('/node/<node_name>/process/<process_name>/restart')
-def json_restart(node_name, process_name):
+def json_restart(node_name, process_name):                                  # 输入主机信息和进程信息
     if session.get('logged_in'):
-        if session['usertype'] == 0 or session['usertype'] == 1:
+        if session['usertype'] == 0 or session['usertype'] == 1:            # 判断用户类型
             try:
                 node_config = Config(CONFIG_FILE).getNodeConfig(node_name)
                 node = Node(node_config)
-                if node.connection.supervisor.stopProcess(process_name):
-                    if node.connection.supervisor.startProcess(process_name):
+                if node.connection.supervisor.stopProcess(process_name):        # 调用supervisor接口停止进程
+                    if node.connection.supervisor.startProcess(process_name):   # 调用接口启动进程
                         add_log = open(ACTIVITY_LOG, "a")
                         add_log.write("%s - %s restarted %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
-                        return JsonValue(process_name, node_name, "restart").success()
+                        return JsonValue(process_name, node_name, "restart").success()  # 如果成功写日志并返回
             except xmlrpclib.Fault as err:
                 add_log = open(ACTIVITY_LOG, "a")
                 add_log.write("%s - %s unsucces restart event %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
-                return JsonValue(process_name, node_name, "restart").error(err.faultCode, err.faultString)
-        else:
+                return JsonValue(process_name, node_name, "restart").error(err.faultCode, err.faultString)  # 如果失败写日志并返回
+        else:   # 用户类型不对
             add_log = open(ACTIVITY_LOG, "a")
             add_log.write("%s - %s is unauthorized user request for restart. Restart event fail for %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
             return jsonify(status = "error2",
                            message = "You are not authorized this action" )
-    else:
+    else:       # 没用登录
         add_log = open(ACTIVITY_LOG, "a")
         add_log.write("%s - Illegal request for restart to %s node's %s process %s .\n"%( datetime.now().ctime(), node_name, process_name ))
         return redirect(url_for('login'))
 
-# Process start
+# 启动进程
 @app.route('/node/<node_name>/process/<process_name>/start')
-def json_start(node_name, process_name):
+def json_start(node_name, process_name):                                    # 输入主机信息和进程信息
     if session.get('logged_in'):
-        if session['usertype'] == 0 or session['usertype'] == 1:
+        if session['usertype'] == 0 or session['usertype'] == 1:            # 判断用户类型
             try:
                 node_config = Config(CONFIG_FILE).getNodeConfig(node_name)
                 node = Node(node_config)
-                if node.connection.supervisor.startProcess(process_name):
+                if node.connection.supervisor.startProcess(process_name):   # 调用接口启动进程
                     add_log = open(ACTIVITY_LOG, "a")
                     add_log.write("%s - %s started %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
-                    return JsonValue(process_name, node_name, "start").success()
+                    return JsonValue(process_name, node_name, "start").success()    # 如果成功写日志并返回
             except xmlrpclib.Fault as err:
                 add_log = open(ACTIVITY_LOG, "a")
                 add_log.write("%s - %s unsucces start event %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
-                return JsonValue(process_name, node_name, "start").error(err.faultCode, err.faultString)
-        else:   
+                return JsonValue(process_name, node_name, "start").error(err.faultCode, err.faultString)    # 如果失败写日志并返回
+        else:   # 用户类型不对
             add_log = open(ACTIVITY_LOG, "a")
             add_log.write("%s - %s is unauthorized user request for start. Start event fail for %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
             return jsonify(status = "error2",
                            message = "You are not authorized this action" )
-    else:
+    else:       # 没用登录
         add_log = open(ACTIVITY_LOG, "a")
         add_log.write("%s - Illegal request for start to %s node's %s process %s .\n"%( datetime.now().ctime(), node_name, process_name ))
         return redirect(url_for('login'))
 
-# Process stop
+# 停止进程
 @app.route('/node/<node_name>/process/<process_name>/stop')
 def json_stop(node_name, process_name):
     if session.get('logged_in'):
@@ -338,20 +349,20 @@ def json_stop(node_name, process_name):
 # Node name list in the configuration file
 @app.route('/node/name/list')
 def getlist():
-    if session.get('logged_in'):
-        node_name_list = Config(CONFIG_FILE).node_list
-        return jsonify( node_name_list = node_name_list )
+    if session.get('logged_in'):    # 是否登录
+        node_name_list = Config(CONFIG_FILE).node_list      # 获取主机列表
+        return jsonify( node_name_list = node_name_list )   # 返回主机列表
     else:
         return redirect(url_for('login'))
 
-# Show log for process
+# 查看进程日志
 @app.route('/node/<node_name>/process/<process_name>/readlog')
 def readlog(node_name, process_name):
     if session.get('logged_in'):
         if session['usertype'] == 0 or session['usertype'] == 1 or session['usertype'] == 2:
             node_config = Config(CONFIG_FILE).getNodeConfig(node_name)
             node = Node(node_config)
-            log = node.connection.supervisor.tailProcessStdoutLog(process_name, 0, 500)[0]
+            log = node.connection.supervisor.tailProcessStdoutLog(process_name, 0, 500)[0]  # 调用接口查看日志
             add_log = open(ACTIVITY_LOG, "a")
             add_log.write("%s - %s read log %s node's %s process .\n"%( datetime.now().ctime(), session['username'], node_name, process_name ))
             return jsonify( status = "success", url="node/"+node_name+"/process/"+process_name+"/read" , log=log)
@@ -364,7 +375,7 @@ def readlog(node_name, process_name):
         add_log.write("%s - Illegal request for read log to %s node's %s process %s .\n"%( datetime.now().ctime(), node_name, process_name ))
         return jsonify( status = "error", message= "First login please")
 
-# Add user method for only admin type user
+# 创建用户(只用admin能操作)
 @app.route('/add/user')
 def add_user():
     if session.get('logged_in'):
@@ -376,7 +387,7 @@ def add_user():
             return jsonify(status = 'error')
 
 
-# Delete user method for only admin type user
+# 删除用户(只用admin能操作)
 @app.route('/delete/user')
 def del_user():
     if session.get('logged_in'):
@@ -399,7 +410,7 @@ def del_user_handler(username):
     if session.get('logged_in'):
         if session['usertype'] == 0:
             if username != "admin":
-                cur = get_db().cursor()
+                cur = get_db().cursor()     # 操作数据库
                 cur.execute("delete from userinfo where username=?",[username])
                 get_db().commit()
                 add_log = open(ACTIVITY_LOG, "a")
@@ -425,7 +436,7 @@ def del_user_handler(username):
 def adduserhandler():
     if session.get('logged_in'):
         if session['usertype'] == 0:
-            username = request.form['username']
+            username = request.form['username']     # 接收传参
             password = request.form['password']
             confirmpassword = request.form['confirmpassword']
 
@@ -442,7 +453,7 @@ def adduserhandler():
                 elif request.form['usertype'] == "Read Only":
                     usertype = 3
 
-                cur = get_db().cursor()
+                cur = get_db().cursor()     # 操作数据库
                 cur.execute("select * from userinfo where username=?",(username,))
                 if not cur.fetchall():
                     if password == confirmpassword:
@@ -474,7 +485,7 @@ def adduserhandler():
                        message = "First login please")
 
 
-
+# 修改密码
 @app.route('/change/password/<username>')
 def changepassword(username):
     if session.get('logged_in'):
@@ -496,7 +507,7 @@ def changepassword(username):
 def changepasswordhandler(username):
     if session.get('logged_in'):
         if session['username'] == username:
-            cur = get_db().cursor()
+            cur = get_db().cursor()     # 操作数据库
             cur.execute("select password from userinfo where username=?",(username,))
             ar=[str(r[0]) for r in cur.fetchall()]
             if request.form['old'] == ar[0]:
@@ -527,14 +538,16 @@ def changepasswordhandler(username):
         add_log.write("%s - Illegal request for change %s 's password event.\n"%( datetime.now().ctime(), username))
         return redirect(url_for('login'))
 
+# 错误页面
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 try:
     if __name__ == '__main__':
-        app.run(debug=True, use_reloader=True, host=HOST, port=PORT)
+        app.run(debug=True, use_reloader=True, host=HOST)
 except xmlrpclib.Fault as err:
     print "A fault occurred"
     print "Fault code: %d" % err.faultCode
     print "Fault string: %s" % err.faultString
+
